6eaf842ebde6b16cb8bed10a62020587fc3e3363
your username <kennethafreelancer@gmail.com>
2019-01-19 21:17:49 +0800

    done

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return pug`
+      div.App
+        header.App-header
+          img.App-logo(src=logo alt= "logo")
+
+          p Edit
+            code src / App.js
+            | and save to reload.
+
+          a.App-link( href = "https://reactjs.org" target = "_blank" rel ="noopener noreferrer")
+            | Learn React
+      `
   }
+
 }
 
 export default App;
